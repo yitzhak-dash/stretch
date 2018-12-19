6ead2ee4904dbcb0a8aec17cdc214ea9ffd90c81
yitzhak <idashevsky@gmail.com>
2018-12-19 11:30:12 +0200

    connector

---
+++ web/data-store/connector.ts
@@ -0,0 +1,30 @@
+import { createConnection, Connection, ConnectionOptions } from 'typeorm';
+import config from 'config';
+
+import { User } from '../entities/user';
+
+
+const minOpt: ConnectionOptions = {
+    ...config.get('mysql'),
+    type: 'mysql'
+};
+
+const opt: ConnectionOptions = {
+    ...minOpt,
+    database: 'test',
+    synchronize: true,
+    entities: [User],
+    logging: ['info', 'log']
+};
+
+export const connect = (): Promise<Connection> => createConnection(opt)
+    .catch(async err => {
+        if (err.code === 'ER_BAD_DB_ERROR') {
+            console.log(err.message);
+            const connection = await createConnection({...minOpt, name: 'temp'});
+            const queryRunner = connection.createQueryRunner();
+            await queryRunner.createDatabase('test', true);
+            return createConnection(opt);
+        }
+        throw err;
+    });
