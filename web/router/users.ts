dd08ecd9cceea20a7188acddf73851e929d80dc3
yitzhak <idashevsky@gmail.com>
2018-12-19 11:31:04 +0200

    store data in the db and fetch it from db

--- web/router/users.ts
+++ web/router/users.ts
@@ -1,20 +1,36 @@
 import router from 'restify-router';
+import { Request } from 'restify';
 import errors from 'restify-errors';
+import { getManager } from 'typeorm';
+
+import { User } from '../entities/user';
 
 const userRouter = new router.Router();
 
-userRouter.get('/user', (req, res, next) => {
-    res.json(['moshe', 'avi']);
+userRouter.get('/user', async (req: Request, res, next) => {
+    const manager = getManager();
+    const users = await manager.find(User);
+    res.json(users);
     next();
 });
 
-userRouter.get('/user/:id', (req, res, next) => {
-    res.json('moshe');
+userRouter.get('/user/:id', async (req: Request, res, next) => {
+    const id = req.params.id;
+    const manager = getManager();
+    const user = await manager.findOne(User, id);
+    res.json(user);
     next();
 });
 
-userRouter.post('/user', (req, res) => {
-    res.json('good');
+userRouter.post('/user', async (req: Request, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        isActive: false
+    };
+    const manager = getManager();
+    const result = await manager.save(User, user);
+    res.json(result);
 });
 
 userRouter.put('/user/:id', (req, res) => {
