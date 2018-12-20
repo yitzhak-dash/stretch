7855cdd8b9706ed97e408c682ce42f627293aea2
yitzhak <idashevsky@gmail.com>
2018-12-20 12:35:51 +0200

    create mock test #14

--- test/user.api.test.ts
+++ test/user.api.test.ts
@@ -19,13 +19,13 @@ afterAll(() => {
 // test('Test for you travis', () => expect(1 === 1).toBeTruthy());
 
 describe('GET /user/:id', () => {
-    test('should return 200 OK', async () => {
+    test('should return 200 OK', () => {
         return request(server)
             .get('/user/1')
             .expect(200);
     });
 
-    test('should return 204 - no content', async () => {
+    test('should return 204 - no content', () => {
         return request(server)
             .get('/user/100000')
             .expect(204);
