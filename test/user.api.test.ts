9d6724159ad980ef4638f0df029e3f706deaa0a8
yitzhak <idashevsky@gmail.com>
2018-12-20 20:53:24 +0200

    create mock test #22

--- test/user.api.test.ts
+++ test/user.api.test.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-// import request from 'supertest';
+import request from 'supertest';
 
 import initApp from '../web/server-rest';
 
@@ -13,18 +13,18 @@ afterAll(() => {
     server.close();
 });
 
-test('Test for you travis', () => expect(1 === 1).toBeTruthy());
+// test('Test for you travis', () => expect(1 === 1).toBeTruthy());
 
-// describe('GET /user/:id', () => {
-// test('should return 200 OK', () => {
-//     return request(server)
-//         .get('/user/1')
-//         .expect(200);
-// });
-//
-// test('should return 204 - no content', () => {
-//     return request(server)
-//         .get('/user/100000')
-//         .expect(204);
-// });
-// });
+describe('GET /user/:id', () => {
+    test('should return 200 OK', () => {
+        return request(server)
+            .get('/user/1')
+            .expect(200);
+    });
+
+    test('should return 204 - no content', () => {
+        return request(server)
+            .get('/user/100000')
+            .expect(204);
+    });
+});
